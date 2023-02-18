287bb719fd12c69f54c86ff363dbb2c7fb561b96
tongxinCode <tongxink@qq.com>
2023-02-18 09:32:53 +0800

    with namespace and mount /proc

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "mydocker",
+		Usage: "mydocker is a simple container runtime implementation.",
+	}
+
+	app.Commands = []*cli.Command{
+		&runCommand, &initCommand,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ run.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"mydocker/container"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func Run(tty bool, command string) {
+	parent := container.NewParentProcess(tty, command)
+	if err := parent.Start(); err != nil {
+		log.Error(err)
+	}
+	parent.Wait()
+	os.Exit(-1)
+}
